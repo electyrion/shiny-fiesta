1f564e5584b260457aca7f32bacb6adb55a47159
Vicky Maulana <muhammad.vicky@ui.ac.id>
2024-08-12 16:45:32 +0700

    feat: Update app name and labels

--- lib/presentation/second/second.dart
+++ lib/presentation/second/second.dart
@@ -1,16 +1,9 @@
-import 'package:boilerplate/data/sharedpref/constants/preferences.dart';
 import 'package:boilerplate/di/service_locator.dart';
-import 'package:boilerplate/presentation/home/store/language/language_store.dart';
 import 'package:boilerplate/presentation/home/store/theme/theme_store.dart';
 import 'package:boilerplate/presentation/user/store/user_store.dart';
 import 'package:boilerplate/presentation/user/user_list.dart';
-import 'package:boilerplate/utils/locale/app_localization.dart';
-import 'package:boilerplate/utils/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:material_dialogs/dialogs.dart';
-import 'package:material_dialogs/widgets/buttons/icon_button.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SecondScreen extends StatefulWidget {
   @override
@@ -20,7 +13,6 @@ class SecondScreen extends StatefulWidget {
 class SecondScreenState extends State<SecondScreen> {
   //stores:---------------------------------------------------------------------
   final ThemeStore _themeStore = getIt<ThemeStore>();
-  final LanguageStore _languageStore = getIt<LanguageStore>();
   final UserStore _userStore = getIt<UserStore>();
 
   @override
@@ -116,6 +108,10 @@ class SecondScreenState extends State<SecondScreen> {
         },
         child: Text(
           "Choose a User",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+          ),
         ),
       ),
     );
@@ -131,9 +127,7 @@ class SecondScreenState extends State<SecondScreen> {
 
   List<Widget> _buildActions(BuildContext context) {
     return <Widget>[
-      _buildLanguageButton(),
       _buildThemeButton(),
-      _buildLogoutButton(),
     ];
   }
 
@@ -149,74 +143,6 @@ class SecondScreenState extends State<SecondScreen> {
           ),
         );
       },
-    );
-  }
-
-  Widget _buildLogoutButton() {
-    return IconButton(
-      onPressed: () {
-        SharedPreferences.getInstance().then((preference) {
-          preference.setBool(Preferences.is_logged_in, false);
-          Navigator.of(context).pushReplacementNamed(Routes.login);
-        });
-      },
-      icon: Icon(
-        Icons.power_settings_new,
-      ),
-    );
-  }
-
-  Widget _buildLanguageButton() {
-    return IconButton(
-      onPressed: () {
-        _showLanguageDialog();
-      },
-      icon: const Icon(Icons.language),
-    );
-  }
-
-  void _showLanguageDialog() {
-    Dialogs.materialDialog(
-      context: context,
-      title: AppLocalizations.of(context).translate('home_tv_choose_language'),
-      color: Theme.of(context).scaffoldBackgroundColor,
-      customView: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: _languageStore.supportedLanguages
-            .map(
-              (object) => ListTile(
-                dense: true,
-                contentPadding: const EdgeInsets.all(0.0),
-                title: Text(
-                  object.language,
-                  style: TextStyle(
-                    color: _languageStore.locale == object.locale
-                        ? Theme.of(context).primaryColor
-                        : _themeStore.darkMode
-                            ? Colors.white
-                            : Colors.black,
-                  ),
-                ),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  // Change user language based on selected locale
-                  _languageStore.changeLanguage(object.locale);
-                },
-              ),
-            )
-            .toList(),
-      ),
-      actions: [
-        IconsButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          text: 'Close',
-          iconData: Icons.close,
-          textStyle: const TextStyle(color: Colors.white),
-          iconColor: Colors.white,
-        ),
-      ],
     );
   }
 }
