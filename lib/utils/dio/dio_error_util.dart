baa78c5f63a5aa59da03c67c621d654c49b52e7c
Vicky Maulana <muhammad.vicky@ui.ac.id>
2024-08-12 22:02:25 +0700

    feat: Update DioError to DioException in network interceptors

--- lib/utils/dio/dio_error_util.dart
+++ lib/utils/dio/dio_error_util.dart
@@ -2,38 +2,31 @@ import 'package:dio/dio.dart';
 
 class DioErrorUtil {
   // general methods:-----------------------------------------------------------
-  static String handleError(DioError error) {
+  static String handleError(DioException error) {
     String errorDescription = "";
-    if (error is DioError) {
-      switch (error.type) {
-        case DioErrorType.cancel:
-          errorDescription = "Request to API server was cancelled";
-          break;
-        case DioErrorType.connectionError:
-        case DioErrorType.connectionTimeout:
-        case DioErrorType.unknown:
-          errorDescription = "Connection timeout with API server";
-          break;
-        case DioErrorType.receiveTimeout:
-          errorDescription = "Receive timeout in connection with API server";
-          break;
-        case DioErrorType.receiveTimeout:
-          errorDescription = "Receive timeout in connection with API server";
-          break;
-        case DioErrorType.badResponse:
-          errorDescription =
-          "Received invalid status code: ${error.response?.statusCode}";
-          break;
-        case DioErrorType.sendTimeout:
-          errorDescription = "Send timeout in connection with API server";
-          break;
-        case DioErrorType.badCertificate:
-          errorDescription = "Incorrect certificate";
-          break;
-      }
-    } else {
-      errorDescription = "Unexpected error occurred";
+    switch (error.type) {
+      case DioExceptionType.cancel:
+        errorDescription = "Request to API server was cancelled";
+        break;
+      case DioExceptionType.connectionError:
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.unknown:
+        errorDescription = "Connection timeout with API server";
+        break;
+      case DioExceptionType.receiveTimeout:
+        errorDescription = "Receive timeout in connection with API server";
+        break;
+      case DioExceptionType.badResponse:
+        errorDescription =
+        "Received invalid status code: ${error.response?.statusCode}";
+        break;
+      case DioExceptionType.sendTimeout:
+        errorDescription = "Send timeout in connection with API server";
+        break;
+      case DioExceptionType.badCertificate:
+        errorDescription = "Incorrect certificate";
+        break;
     }
-    return errorDescription;
+      return errorDescription;
   }
 }
